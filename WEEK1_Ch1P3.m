c0f6fad34448190a67ac351a6c5d734e6129cf88
Maddie <mdsn.cgswll@gmail.com>
2016-04-02 00:04:38 -0700

    Added files via upload

---
+++ WEEK1_Ch1P3.m
@@ -0,0 +1,6 @@
+%Chapter Exercise 1.3
+%Assign a value to the variable x on the command line, e.g., x=4*pi^2. What
+%is the square root of x? What is the cosine of the square root of x?
+x=pi*6^2;
+A=sqrt(x)
+B=cos(A)
